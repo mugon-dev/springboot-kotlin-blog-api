8ebd7bfa52b02846e18065614b04487037c0b1cd
jmg <jmg41490400@gmail.com>
2022-07-20 16:48:26 +0900

    add post all with category api

--- src/main/kotlin/com/conny/blog/repository/PostRepository.kt
+++ src/main/kotlin/com/conny/blog/repository/PostRepository.kt
@@ -4,11 +4,16 @@ import com.conny.blog.model.entity.PostEntity
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 interface PostRepository : JpaRepository<PostEntity, Long> {
     fun queryByIdAndDeletedAtIsNull(id: Long): PostEntity
     fun queryAllByAndDeletedAtIsNull(): MutableList<PostEntity>
+
     fun queryAllByDeletedAtIsNull(pageable: Pageable): Page<PostEntity>
+
+    @Query("select p from PostEntity  p where p.category.id = ?1 and p.deletedAt is null")
+    fun queryAllByCategoryIdAndDeletedAtIsNull(categoryId: Long, pageable: Pageable): Page<PostEntity>
 }
--- src/main/kotlin/com/conny/blog/web/controller/frontend/FrontendPostController.kt
+++ src/main/kotlin/com/conny/blog/web/controller/frontend/FrontendPostController.kt
@@ -55,8 +55,9 @@ class FrontendPostController @Autowired constructor(
         return BodyResponse.success(PostResponse.toEntity(data), message = "Soft Delete one Successful")
     }
 
-    @GetMapping("/page")
+    @GetMapping("/all/{categoryId}")
     fun findAllAvailableByPage(
+        @PathVariable categoryId: Long,
         @RequestParam(required = false, defaultValue = "0") page: Int,
         @RequestParam(required = false, defaultValue = "20") size: Int,
         @RequestParam(required = false, defaultValue = "id") sort: String,
@@ -64,7 +65,7 @@ class FrontendPostController @Autowired constructor(
         @RequestParam(required = false, defaultValue = "false") paged: Boolean,
     ): ResponseEntity<Any> {
         val data = if (paged) {
-            postService.findAllAvailable(Pageable.unpaged()).map {
+            postService.findAllAvailable(categoryId, Pageable.unpaged()).map {
                 PostResponse.toEntity(it)
             }
         } else {
@@ -74,8 +75,14 @@ class FrontendPostController @Autowired constructor(
             } else {
                 Sort.by(sort).ascending()
             }
-            postService.findAllAvailable(PageRequest.of(page, size, sb)).map {
-                PostResponse.toEntity(it)
+            if (categoryId == 0L) {
+                postService.findAllAvailable(categoryId, PageRequest.of(page, size, sb)).map {
+                    PostResponse.toEntity(it)
+                }
+            } else {
+                postService.findAllAvailable(categoryId, PageRequest.of(page, size, sb)).map {
+                    PostResponse.toEntity(it)
+                }
             }
         }
         return BodyResponse.success(data, message = "Find all Successful")
